553925aa3a6ea2b62c9223dcba3edf048f349516
saurabh <sourabhmahra91@gmail.com>
2025-06-05 08:54:06 +0530

    attempt to support valkey on render 2

--- pulse/memory.py
+++ pulse/memory.py
@@ -2,11 +2,4 @@ import os
 
 import redis
 
-REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
-
-valkey_client = redis.Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    decode_responses=True
-)
+valkey_client = redis.from_url(os.environ["REDIS_URL"])
